ba6c7a7cf2c765979bd5d879f4d06ac31736de0b
carme <carmecodina.p@gmail.com>
2021-11-11 09:54:35 +0100

    exercici 1

---
+++ src/components/Escena.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Escena = () => {
+    return (
+        <p>
+            El nostre heroi estava surant per l'espai sideral quan a la
+            llunyania va albirar una nau espacial.
+        </p>
+    );
+};
+
+export default Escena;
